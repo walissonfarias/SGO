21059f601278a953b2d3eee83b84e88d84f022cf
walissonff <walissonj8027@hotmail.com>
2017-10-24 07:59:12 -0200

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+tcc
+===
+
+A Symfony project created on October 20, 2017, 7:52 am.
+# SGO
---
+++ src/AppBundle/Entity/Usuario.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Usuario
+ *
+ * @ORM\Table(name="usuario")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\UsuarioRepository")
+ */
+class Usuario
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nome", type="string", length=255)
+     */
+    private $nome;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="idade", type="integer")
+     */
+    private $idade;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nome
+     *
+     * @param string $nome
+     *
+     * @return Usuario
+     */
+    public function setNome($nome)
+    {
+        $this->nome = $nome;
+
+        return $this;
+    }
+
+    /**
+     * Get nome
+     *
+     * @return string
+     */
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+    /**
+     * Set idade
+     *
+     * @param integer $idade
+     *
+     * @return Usuario
+     */
+    public function setIdade($idade)
+    {
+        $this->idade = $idade;
+
+        return $this;
+    }
+
+    /**
+     * Get idade
+     *
+     * @return int
+     */
+    public function getIdade()
+    {
+        return $this->idade;
+    }
+}
+
